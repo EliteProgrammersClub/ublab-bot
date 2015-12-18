1f0c1a6770375b6808c186444853555cd05d2bf2
Nyah Check <check.nyah@gmail.com>
2015-12-18 14:12:50 +0100

    Updates to licence

--- src/main/java/org/ublab/bot/DccFileTransfer.java
+++ src/main/java/org/ublab/bot/DccFileTransfer.java
@@ -1,18 +1,23 @@
-/* 
-Copyright Paul James Mutton, 2001-2009, http://www.jibble.org/
-
-This file is part of PircBot.
-
-This software is dual-licensed, allowing you to choose between the GNU
-General Public License (GPL) and the www.jibble.org Commercial License.
-Since the GPL may be too restrictive for use in a proprietary application,
-a commercial license is also provided. Full license information can be
-found at http://www.jibble.org/licenses/
-
-*/
-
-
-package org.jibble.pircbot;
+/**
+ *  This file is part of the ublab-bot
+ *  Copyright (C) 2015  Black Hackers(Elite Programmers Club, University of Buea)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+package org.ublab.bot;
 
 import java.net.*;
 import java.io.*;
